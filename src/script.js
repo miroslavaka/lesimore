70227db888a30b9d229e98499b7f8aa84932ddb1
Miroslava Kacmarova <kacmarova.miroslava@gmail.com>
2022-06-19 22:30:28 +0200

    page about, services, contact

--- src/script.js
+++ src/script.js
@@ -1,2 +1,16 @@
 let currYear = document.querySelector('.year');
 currYear.innerHTML = new Date().getFullYear();
+
+const hamburger = document.querySelector('.menu__hamburger');
+const menu = document.querySelector('.menu__list');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+document.querySelectorAll('.menu__link').forEach((link) =>
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }),
+);
